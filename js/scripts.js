24585e9754d8b43d820aaf1ff390357f5f0fe12d
matthewrobison818 <matthewrobison818@gmail.com>
2017-02-01 16:53:21 -0800

    Write cooking app scripts

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var gallon = parseInt(prompt("How many gallons would you like to convert into liters?"));
+
+var gallonConverted = gallon * 3.78541;
+
+var result = alert("" + gallon + " gallons " + "equal " + gallonConverted + " liters.");
+
+var liter = parseInt(prompt("How many liters would you like to convert into gallons?"));
+
+var convertedLiter = liter / 3.78541;
+
+var resultLiter = alert("" + liter + " liters" + " equals " + convertedLiter + " gallons.");
